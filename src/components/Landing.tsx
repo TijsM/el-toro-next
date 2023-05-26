0995cfed2d266c0168ef4b294cf4c13bb27582ce
TijsM <tijs.martens.tijs@gmail.com>
2023-05-26 23:00:02 +0200

    feat: add reset css

---
+++ src/components/Landing.tsx
@@ -0,0 +1,16 @@
+export const Landing = () => {
+  return (
+    <header>
+      <h1>Retro Grote Prijs El Toro</h1>
+      <h2>4 September 2022</h2>
+      <div>
+        Een koers waar winnen ondergeschikt is aan vertier, waar gestart wordt
+        in retro outfit en waar we van klikpedalen en dikke zanten niet moeten
+        weten.
+      </div>
+      <a data-scroll href="#inschrijvingen">
+        Inschrijven
+      </a>
+    </header>
+  );
+};
