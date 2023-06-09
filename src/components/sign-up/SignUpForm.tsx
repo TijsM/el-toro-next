08ed959fd7c7d12eac90bdfa22022cf5ad444973
TijsM <tijs.martens.tijs@gmail.com>
2023-06-09 16:08:28 +0200

    feat: add Particpant form

--- src/components/sign-up/SignUpForm.tsx
+++ src/components/sign-up/SignUpForm.tsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import { Input } from "../core";
 import styled from "styled-components";
 import { breakpoints } from "../../constants/breakpoints";
+import {
+  ParticipantForm,
+  Particpant,
+  defaultParticipant,
+} from "./ParticipantForm";
 
 export const SignUpForm = () => {
   const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [socialSecurityNumber, setSocialSecurityNumber] = useState("");
-  const [placeOfBirth, setPlaceOfBirth] = useState("");
-  const [city, setCity] = useState("");
+
+  const [participant, setParticipant] =
+    useState<Particpant>(defaultParticipant);
 
   return (
     <StContainer>
@@ -20,32 +24,10 @@ export const SignUpForm = () => {
           name="email"
         />
       </StGeneralInfoContainer>
-      <StParticipantContainer>
-        <Input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          label={"Name"}
-          name="name"
-        />
-        <Input
-          value={socialSecurityNumber}
-          onChange={(e) => setSocialSecurityNumber(e.target.value)}
-          label={"Social Security Number"}
-          name="socialSecurityNumber"
-        />
-        <Input
-          value={placeOfBirth}
-          onChange={(e) => setPlaceOfBirth(e.target.value)}
-          label={"Place of Birth"}
-          name="placeOfBirth"
-        />
-        <Input
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          label={"City"}
-          name="city"
-        />
-      </StParticipantContainer>
+      <ParticipantForm
+        participant={participant}
+        setParticipant={setParticipant}
+      />
     </StContainer>
   );
 };
@@ -70,8 +52,5 @@ const StContainer = styled.div`
 `;
 
 const StGeneralInfoContainer = styled.div`
-  padding-bottom: 48px;
-`;
-const StParticipantContainer = styled.div`
   padding-bottom: 48px;
 `;
