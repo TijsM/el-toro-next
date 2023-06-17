f801689e3db3e66b97814f20c0739ad353f29063
TijsM <tijs.martens.tijs@gmail.com>
2023-06-17 15:00:30 +0200

    feat: add status

---
+++ src/app/status/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styled from "styled-components";
+
+export default function Page() {
+  return <StTest>Site is up and running</StTest>;
+}
+
+const StTest = styled.div`
+  color: white;
+  z-index: 100;
+`;
